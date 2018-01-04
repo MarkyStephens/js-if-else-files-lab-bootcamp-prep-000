00c283d8fe29c9226bded3a79f07c926db82de9e
Mark Stephens <mstephens@trackeleven.com>
2018-01-04 21:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var name = "Joe";
 var height = "74";
 if ("name" === "Susans") {
   console.log("The name is correct");
-}
+} else {
+  console.log("Expected the name to be Susan");
+} 
 
 // Don't worry about this
 module.exports = { name, height
